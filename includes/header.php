d5c5d5c41b767668814a909f0d7bac9c263dc3cd
Willian Costa Souza <willian.93.costa@gmail.com>
2012-12-01 15:11:37 -0200

    struct

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,8 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/main.css">
         
         <script src="js/vendor/modernizr-2.6.1.min.js"></script>
     </head>
--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php include 'includes/header.php'; ?>
 
+<div class="container">
+	<header class="row">
+		<div class="span3">encomendas</div>
+		<div class="span4">logo</div>
+		<div class="span3">siga-nos</div>
+		<menu>
+			<ul>
+				<li><a href="">1</a></li>
+				<li><a href="">2</a></li>
+				<li><a href="">3</a></li>
+				<li><a href="">4</a></li>
+				<li><a href="">5</a></li>
+				<li><a href="">6</a></li>
+			</ul>
+		</menu>
+	</header>
+
+	<section class="row">
+		<aside class="span2">video e novidade</aside>
+		<article class="span8">banner</article>
+		<footer class="span10">carousel</footer>
+	</section>
+
+	<footer class="row">
+		<div class="span5">form</div>
+		<div class="span5">maps</div>
+	</footer>
+</div>
+
 <?php include 'includes/footer.php'; ?>
