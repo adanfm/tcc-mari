bee2929e20918b016b78b6bc822f79407fe2ca63
Adan Felipe Medeiros <adan.grg@gmail.com>
2012-12-01 20:58:58 -0200

    Finalizado pagina de contatos

--- contato.php
+++ contato.php
@@ -1,36 +1,63 @@
 <?php include 'includes/header.php'; ?>
 
-<form class="form-horizontal">
-  <!-- Nome -->
-  <div class="control-group">
-    <label class="control-label" for="inputNome">Nome</label>
-    <div class="controls">
-      <input type="text" id="inputNome" placeholder="Nome">
-    </div>
-  </div>
-  <!-- Email -->
-  <div class="control-group">
-    <label class="control-label" for="inputEmail">Email</label>
-    <div class="controls">
-      <input type="text" id="inputEmail" placeholder="Email">
-    </div>
-  </div>
-  <!-- Assunto -->
-  <div class="control-group">
-    <label class="control-label" for="inputAssunto">Assunto</label>
-    <div class="controls">
-      <input type="text" id="inputAssunto" placeholder="Assunto">
-    </div>
-  </div>
-  <div class="control-group">
-    <div class="controls">
-      <label class="textarea">  
-        <textarea rows="5"></textarea>
-      </label>
-      
-      <button type="submit" class="btn btn-warning">Enviar</button>
-    </div>
-  </div>
-</form>
+<div class="container">
+	<header class="row">
+		<div class="span10 offset1 header">
+			<div class="span5 encomendas">
+				<img src="img/encomendas.png" alt="Encomendas">
+			</div>
+			<div class="logo">
+				<img src="img/logo.png" alt="Sabor di Amore">
+			</div>
+			<div class="siga-nos">
+				<img src="img/siga-nos.png" alt="Siga-nos">
+				<a href="#">
+					<img src="img/twitter.png" alt="Twitter">
+				</a>
+				<a href="#">
+					<img src="img/facebook.png" alt="Facebook">
+				</a>
+			</div>
+		</div>
+		<div class="span12">
+			<menu class="no-margin">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Produtos</a></li>
+					<li><a href="">Serviços</a></li>
+					<li style="padding-right: 56px;"><a href="">Galeria</a></li>
+					<li><a href="">Quem somos</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</menu>
+		</div>
+	</header>
+    <section class="section-contato">
+        <div class="span5 form-contato">
+            <h4>Contato</h4>
+            <div class="form-contato-info">
+                <p>Fone: <span>47 3028.3848|47 9631.0999</span></p>
+                <p>email: atendimento@sabordlamore.com.br</p>
+                <p>Endereço: Rua Corupá,nº138-Joinville/SC</p>
+                <p class="destaque">Atendimento com hora Marcada!</p>
+            </div>
+            <form class="form-horizontal" action="#" method="post">
+                    <label>Nome:</label>
+                    <input type="text" name="name" class="input-block-level"/>
+                    <label>Email:</label>
+                    <input type="text" name="email" class="input-block-level"/>
+                    <label>Mensagem:</label>
+                    <textarea name="message" class="input-block-level textarea-footer"></textarea>
+                    <br />
+                    <input type="submit" value="Enviar" class="form-contato-submit"/>
 
-<?php include 'includes/footer.php'; ?>
+             </form>
+        </div>
+        <div class="span5">
+            <h4>Localização</h4>
+            <div class="form-contato-maps"></div>
+            <button>Exibir mapa Ampliado</button>
+        </div>
+    </section>
+</div>
+<?php include 'includes/footer-contato.php';
---
+++ includes/footer-contato.php
@@ -0,0 +1,51 @@
+	<footer class="row footer">
+        <div class="container">
+            <div class="span5 cadastro-email">
+                <span>Cadastre-se</span> informando seu nome  e email,<br />
+                assim poderemos fazer contato sempre que <br />
+                tivermos novidade!<br /><br />
+
+                <form class="form-horizontal" action="#" method="post">
+                    <label>Nome:</label>
+                    <input type="text" name="name" class="input-block-level"/>
+                    <label>Email:</label>
+                    <input type="text" name="email" class="input-block-level"/>
+                    <br />
+                    <input type="submit" value="Enviar" class="submit-form"/>
+                </form>
+            </div>
+        
+            <div class="span5">
+                <h4>Indique:</h4>
+                <form class="form-inline">
+                    <label style="margin-left:15px; ">De:</label>
+                    <input type="text" class="input-indicacao"/><br />
+                    <label>Email:</label>
+                    <input type="text" class="input-indicacao"/><br />
+                    <div class="indicacao">
+                        <label style="margin-left:4px; ">Para:</label>
+                        <input type="text" class="input-indicacao"/><br />
+                        <label>Email:</label>
+                        <input type="text" class="input-indicacao"/>
+                    </div>
+                    <input type="submit" value="Enviar" class="submit-form"/>
+                </form>
+                
+            </div>
+        </div>
+	</footer>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
