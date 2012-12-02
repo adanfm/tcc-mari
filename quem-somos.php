ca8b1a4c8ffc9483206a83213713f5987dd63c70
Fernando Mariano <fernando.mar16@gmail.com>
2012-12-02 15:17:43 -0200

    Alterações do layout

--- quem-somos.php
+++ quem-somos.php
@@ -1,3 +1,37 @@
 <?php include 'includes/header.php'; ?>
 
+<div class="container">
+	<header class="row">
+		<div class="span10 offset1 header">
+			<div class="span5 encomendas">
+				<img src="img/encomendas.png" alt="Encomendas">
+			</div>
+			<div class="logo">
+				<img src="img/logo.png" alt="Sabor di Amore">
+			</div>
+			<div class="siga-nos">
+				<img src="img/siga-nos.png" alt="Siga-nos">
+				<a href="#">
+					<img src="img/twitter.png" alt="Twitter">
+				</a>
+				<a href="#">
+					<img src="img/facebook.png" alt="Facebook">
+				</a>
+			</div>
+		</div>
+		<div class="span12">
+			<menu class="no-margin">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Produtos</a></li>
+					<li><a href="">Serviços</a></li>
+					<li style="padding-right: 56px;"><a href="">Galeria</a></li>
+					<li><a href="">Quem somos</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</menu>
+		</div>
+	</header>
+</div>
+
 <?php include 'includes/footer.php'; ?>
